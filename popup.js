e87df9cdd39598a9a40902b76eddba76d799cc7b
David <deivi7.hs@gmail.com>
2025-03-14 09:02:22 +0100

    first commit

---
+++ popup.js
@@ -0,0 +1,97 @@
+document.getElementById("pendingBtn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: modifySelectPending
+    });
+  });
+});
+
+document.getElementById("setUserNotes").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: setUserNotes
+    });
+  });
+});
+
+
+function setUserNotes() {
+  //let selects = document.querySelectorAll("select"); /**:not([disabled]) */
+  const table = document.querySelector('table[data-st-table="qualificacions"]');
+  let data =[];
+  if (table) {
+      // Recorremos todas las filas de la tabla (exceptuando la cabecera si tiene)
+      table.querySelectorAll("tr").forEach(tr => {
+          const tds = tr.querySelectorAll("td");
+        
+          // Verificamos si hay al menos dos columnas en la fila
+          if (tds.length > 1) {
+              tds[0].classList.add("bg-danger", "text-white"); // Fondo rojo, texto blanco
+              // alert( tds[0].textContent.trim());
+              let select = tds[3].querySelector('select');
+              let value = "string:PDT";
+
+               // Extraemos código del módulo y código del RA
+               let parts = tds[0].textContent.trim().split("_");
+               let moduleCode = parts[0];
+               let raCode = parts.length > 2 ? parts[2] : "GENERAL";
+               console.log(moduleCode+'-'+raCode);
+               
+               // Inicializamos el array si no existe
+               if (!data[moduleCode]) {
+                   data[moduleCode] = [];
+               }
+               
+               if (!data[moduleCode][raCode]) {
+                   data[moduleCode][raCode] = [];
+               }
+               
+               // Almacenamos el select en la estructura
+               data[moduleCode][raCode].push(tds[0].textContent.trim());
+
+              let optionExists = Array.from(select.options).some(option => option.value === value);
+              let event;
+              if (optionExists /* && !select.value*/) {
+                  select.value = value;
+                  event = new Event('change');
+                  select.dispatchEvent(event);
+              }    
+          
+              //setSelectValue(select, "string:PDT");
+              //setSelectValue(select, "string:PQ");
+            
+              tds[1].classList.add("bg-primary", "text-white"); // Fondo azul, texto blanco
+          }
+      });
+
+      console.log(data);
+  }
+
+  };
+
+
+function modifySelectPending() {
+  let selects = document.querySelectorAll("select"); /**:not([disabled]) */
+  alert('Response from background: ' + selects.length);
+
+  selects.forEach(select => {
+      let optionExists = Array.from(select.options).some(option => option.value === "string:PDT");
+      let event;
+      if (optionExists /* && !select.value*/) {
+          select.value = "string:PDT";
+          event = new Event('change');
+          select.dispatchEvent(event);
+      }
+
+      optionExists = Array.from(select.options).some(option => option.value === "string:PQ");
+      if (optionExists /*&& !select.value*/) {
+          select.value = "string:PQ";
+          event = new Event('change');
+          select.dispatchEvent(event);
+      }
+  });
+        
+}
+
