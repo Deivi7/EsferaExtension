753403cac6241b54ab6a9aeb38de1f5e6eb95f4a
David <deivi7.hs@gmail.com>
2025-03-15 11:49:37 +0100

    Texta Area

--- popup.js
+++ popup.js
@@ -2,32 +2,47 @@ document.getElementById("pendingBtn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      function: modifySelectPending
+      function: modifySelect,
+      args : [ "string:PDT", getForcePending() ]
     });
   });
 });
 
-function getJsonText() { 
-  return  document.getElementById("userNotesText").value; // Obtiene el JSON del textarea
- }
+document.getElementById("processBtn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: modifySelect,
+      args : [ "string:EP", getForceProcess() ]
+    });
+  });
+});
 
 document.getElementById("setUserNotes").addEventListener("click", () => {
-  
-  
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-
-  
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id, },
       function: setUserNotes,
       args : [ getJsonText() ],
     });
-
   });
 });
 
+function getForcePending() { 
+  return  document.getElementById("forcePending").checked; // Obtiene el JSON del textarea
+}
+
+function getForceProcess() { 
+  return  document.getElementById("forceProcess").checked; // Obtiene el JSON del textarea
+}
+
+function getJsonText() { 
+  return  document.getElementById("userNotesText").value; // Obtiene el JSON del textarea
+ }
+
 
 function setUserNotes(jsonText) {
+  
   let jsonData;
   try {
      jsonData = JSON.parse(jsonText); // Parseamos el JSON
@@ -35,12 +50,12 @@ function setUserNotes(jsonText) {
     alert("Error al parsear el JSON. Asegúrate de que esté en el formato correcto.");
   }
  
- 
   //let selects = document.querySelectorAll("select"); /**:not([disabled]) */
   const table = document.querySelector('table[data-st-table="qualificacions"]');
   let data =[];
   let select;
   let event;
+  let input
 
   if (table) {
       // Recorremos todas las filas de la tabla (exceptuando la cabecera si tiene)
@@ -52,52 +67,34 @@ function setUserNotes(jsonText) {
               tds[0].classList.add("bg-danger", "text-white"); // Fondo rojo, texto blanco
               // alert( tds[0].textContent.trim());
               select = tds[3].querySelector('select');
-              let value = "string:PDT";
+              input = tds[3].querySelector('input');
+              
+              // Extraemos código del módulo y código del RA
+              let parts = tds[0].textContent.trim().split("_");
+              let moduleCode = parts[0];
+              let raCode = parts.length > 2 ? parts[2] : "T";
+              select.id = moduleCode + "_" + raCode;
+              if(input){
+                input.id = "i_"+moduleCode + "_T";
+              }
 
-               // Extraemos código del módulo y código del RA
-               let parts = tds[0].textContent.trim().split("_");
-               let moduleCode = parts[0];
-               let raCode = parts.length > 2 ? parts[2] : "T";
-               select.id = moduleCode + "_" + raCode;
-               
-               // Inicializamos el array si no existe
-               if (!data[moduleCode]) {
-                   data[moduleCode] = [];
-               }
-               
-               if (!data[moduleCode][raCode]) {
-                   data[moduleCode][raCode] = [];
-               }
-               
-               // Almacenamos el select en la estructura
-               data[moduleCode][raCode].push(select);
-              //console.log(select);
-    
-              // let optionExists = Array.from(select.options).some(option => option.value === value);
-              // let event;
-              // if (optionExists /* && !select.value*/) {
-              //     select.value = value;
-              //     event = new Event('change');
-              //     select.dispatchEvent(event);
-              // }    
-          
-              //setSelectValue(select, "string:PDT");
-              //setSelectValue(select, "string:PQ");
-            
               tds[1].classList.add("bg-primary", "text-white"); // Fondo azul, texto blanco
           }
       });
 
-
-  
       jsonData.forEach((entry) => {
         const modCode = entry.mod;
         const raCode = entry.ra;
         const nota = entry.nota;
-         // Accede al select correspondiente
 
         let value="";
-        if (nota === "") {
+        if (nota === "" && raCode =="T") {
+          value = "string:PQ";
+        } else if (nota != "" && raCode == "T") {
+          value = "";
+          console.log("======");
+          console.log(value);
+        } else if (nota === "") {
           value = "string:PDT";
         } else if (nota === "P") {
           value = "string:EP";
@@ -106,49 +103,55 @@ function setUserNotes(jsonText) {
         } else {
           value = "string:A"+nota;
         }
-        console.log("======");
-        console.log(value);
+       
         select = document.getElementById(modCode + "_" + raCode)
-        
+
+        if (nota != "" && raCode == "T") {
+          input = document.getElementById("i_" + modCode + "_" + raCode)
+          if(input){
+            input.value = nota;
+            event = new Event('change');
+            input.dispatchEvent(event);
+          }
+        }else if( raCode == "T" ){
+          input = document.getElementById("i_" + modCode + "_" + raCode)
+          if(input){
+            input.value = "";
+            event = new Event('change');
+            input.dispatchEvent(event);
+          }
+        }
+
         if (select){
-          console.log(select);
           let optionExists = Array.from(select.options).some(option => option.value === value);
           console.log(optionExists);
           if (optionExists /* && !select.value*/) {
-            console.log("entra");
             select.value = value;
             event = new Event('change');
             select.dispatchEvent(event);
           }    
         }
-        
-
-
       });
 
-
     }
-
-      console.log(data);
-
+        
   };
 
 
-function modifySelectPending() {
-  let selects = document.querySelectorAll("select"); /**:not([disabled]) */
-  alert('Response from background: ' + selects.length);
+function modifySelect(state, force) {
 
+  let selects = document.querySelectorAll("select"); /**:not([disabled]) */
   selects.forEach(select => {
-      let optionExists = Array.from(select.options).some(option => option.value === "string:PDT");
+      let optionExists = Array.from(select.options).some(option => option.value === state);
       let event;
-      if (optionExists /* && !select.value*/) {
-          select.value = "string:PDT";
+      if (optionExists  && ( !select.value || force) ) {
+          select.value = state;
           event = new Event('change');
           select.dispatchEvent(event);
       }
 
       optionExists = Array.from(select.options).some(option => option.value === "string:PQ");
-      if (optionExists /*&& !select.value*/) {
+      if (optionExists && ( !select.value || force)) {
           select.value = "string:PQ";
           event = new Event('change');
           select.dispatchEvent(event);
