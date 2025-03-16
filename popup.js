f9dfdc938d73c3a5acacae5ec51371e31624f14d
David <deivi7.hs@gmail.com>
2025-03-16 12:47:39 +0100

    Falta  mirar si select esta desabilitat

--- popup.js
+++ popup.js
@@ -1,3 +1,56 @@
+let studentCode = "";
+let studentName = "";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.storage.local.get(["notes"], (result) => {
+    savedData = result.notes || "";
+    document.getElementById("userNotesText").value = savedData;
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: extractStudentInfo
+  }, (results) => {
+    if (results && results[0] && results[0].result) {
+      const { code, name } = results[0].result;
+      studentCode = code;
+      studentName = name;
+      document.getElementById("idaluesfera").textContent = `IdAlu: ${studentCode}`;
+      document.getElementById("nomesfera").textContent = `Nom: ${studentName}`;
+
+    }
+  });
+
+});
+
+document.getElementById("clearButton").addEventListener("click", () => {
+  document.getElementById("userNotesText").value = "";
+  chrome.storage.local.set({ "notes": "" });
+});
+
+ document.getElementById("userNotesText").addEventListener("input", () => {
+  try {
+    savedData = document.getElementById("userNotesText").value;
+    chrome.storage.local.set({ "notes": savedData });
+  } catch (error) {
+    alert(error);
+    console.error("Error al guardar", error);
+  }
+});
+
+document.getElementById("setRANotes").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: setRANotes,
+      args : [ studentCode ]
+    });
+  });
+});
+
 document.getElementById("pendingBtn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
@@ -19,70 +72,79 @@ document.getElementById("processBtn").addEventListener("click", () => {
 });
 
 document.getElementById("setUserNotes").addEventListener("click", () => {
+
+  let forceUserNotes = document.getElementById("forceUserNotes").checked;
+
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id, },
       function: setUserNotes,
-      args : [ getJsonText() ],
-    });
+      args : [ getJsonText(), studentCode, forceUserNotes ],
+    }, (results) => {
+     
+      if (results && results[0] && results[0].result) {
+        document.getElementById("results").textContent = results[0].result;
+      }
+    }
+  );
   });
 });
 
 function getForcePending() { 
-  return  document.getElementById("forcePending").checked; // Obtiene el JSON del textarea
+  return  document.getElementById("forcePending").checked;
 }
 
 function getForceProcess() { 
-  return  document.getElementById("forceProcess").checked; // Obtiene el JSON del textarea
+  return  document.getElementById("forceProcess").checked;
 }
 
 function getJsonText() { 
-  return  document.getElementById("userNotesText").value; // Obtiene el JSON del textarea
- }
+  return  document.getElementById("userNotesText").value;
+}
 
 
-function setUserNotes(jsonText) {
-  
+function setUserNotes(jsonText, studentCode, force) {
+
   let jsonData;
   try {
      jsonData = JSON.parse(jsonText); // Parseamos el JSON
   } catch (error) {
-    alert("Error al parsear el JSON. Asegúrate de que esté en el formato correcto.");
+    return "Error en analitzar el JSON. Assegura't que estigui en el format correcte.";
   }
- 
-  //let selects = document.querySelectorAll("select"); /**:not([disabled]) */
+  
+  const student = jsonData.find(al => al.idalu == studentCode);
+  if (!student) {
+      return "Alumne no trobat";
+  }
+  
   const table = document.querySelector('table[data-st-table="qualificacions"]');
-  let data =[];
   let select;
   let event;
   let input
 
   if (table) {
-      // Recorremos todas las filas de la tabla (exceptuando la cabecera si tiene)
+    
       table.querySelectorAll("tr").forEach(tr => {
-          const tds = tr.querySelectorAll("td");
-        
-          // Verificamos si hay al menos dos columnas en la fila
-          if (tds.length > 1) {
-              tds[0].classList.add("bg-danger", "text-white"); // Fondo rojo, texto blanco
-              // alert( tds[0].textContent.trim());
-              select = tds[3].querySelector('select');
-              input = tds[3].querySelector('input');
-              
-              // Extraemos código del módulo y código del RA
-              let parts = tds[0].textContent.trim().split("_");
-              let moduleCode = parts[0];
-              let raCode = parts.length > 2 ? parts[2] : "T";
-              select.id = moduleCode + "_" + raCode;
-              if(input){
-                input.id = "i_"+moduleCode + "_T";
-              }
-
-              tds[1].classList.add("bg-primary", "text-white"); // Fondo azul, texto blanco
+        const tds = tr.querySelectorAll("td");
+      
+        if (tds.length > 1) {
+          // tds[0].classList.add("bg-danger", "text-white"); 
+          // tds[1].classList.add("bg-primary", "text-white"); // Fondo azul, texto blanco
+          select = tds[3].querySelector('select');
+          input = tds[3].querySelector('input');
+          
+          let parts = tds[0].textContent.trim().split("_");
+          let moduleCode = parts[0];
+          let raCode = parts.length > 2 ? parts[2] : "T";
+          select.id = moduleCode + "_" + raCode;
+          if(input){
+            input.id = "i_"+moduleCode + "_T";
           }
+        }
       });
+  
+      student.notes.forEach((entry) => {
 
-      jsonData.forEach((entry) => {
         const modCode = entry.mod;
         const raCode = entry.ra;
         const nota = entry.nota;
@@ -92,8 +154,6 @@ function setUserNotes(jsonText) {
           value = "string:PQ";
         } else if (nota != "" && raCode == "T") {
           value = "";
-          console.log("======");
-          console.log(value);
         } else if (nota === "") {
           value = "string:PDT";
         } else if (nota === "P") {
@@ -103,45 +163,75 @@ function setUserNotes(jsonText) {
         } else {
           value = "string:A"+nota;
         }
-       
-        select = document.getElementById(modCode + "_" + raCode)
-
-        if (nota != "" && raCode == "T") {
-          input = document.getElementById("i_" + modCode + "_" + raCode)
-          if(input){
-            input.value = nota;
-            event = new Event('change');
-            input.dispatchEvent(event);
-          }
-        }else if( raCode == "T" ){
-          input = document.getElementById("i_" + modCode + "_" + raCode)
-          if(input){
-            input.value = "";
-            event = new Event('change');
-            input.dispatchEvent(event);
-          }
-        }
-
-        if (select){
-          let optionExists = Array.from(select.options).some(option => option.value === value);
-          console.log(optionExists);
-          if (optionExists /* && !select.value*/) {
-            select.value = value;
-            event = new Event('change');
-            select.dispatchEvent(event);
-          }    
-        }
-      });
-
-    }
         
-  };
+        select = document.getElementById(modCode + "_" + raCode);
+        let selectIsDisabled = select && false; //TODO Mirar si select esta desabilitat
+
+        if(select && !selectIsDisabled){
+    
+          if (nota != "" && raCode == "T") {
+
+            let optionExists = Array.from(select.options).some(option => option.value === value);
+            if (optionExists && ( !select.value || force)) {
+              select.value = value;
+              event = new Event('change');
+              select.dispatchEvent(event);
+            }   
+
+            input = document.getElementById("i_" + modCode + "_" + raCode)
+            
+            if(input  && force){
+              input.value = nota;
+              event = new Event('change');
+              input.dispatchEvent(event);
+            }
+            
+          }else if( raCode == "T" ){
+            input = document.getElementById("i_" + modCode + "_" + raCode)
+            if(input  &&  force){
+              input.value = "";
+              event = new Event('change');
+              input.dispatchEvent(event);
+            }
+          }else if (select){
+            let optionExists = Array.from(select.options).some(option => option.value === value);
+            if (optionExists && ( !select.value || force)) {
+              select.value = value;
+              event = new Event('change');
+              select.dispatchEvent(event);
+            }    
+          }
+
+        }
+
+      });
+      
+      return "Notes de l'alumne "+ student.nomalu +" assignades";
+    }
+    return "Error a llegir la informació de l'Esfer@";
+    
+};
+
+
+function setRANotes(studentCode) {
+
+  alert(studentCode);
+        
+};
 
 
 function modifySelect(state, force) {
+  const table = document.querySelector('table[data-st-table="qualificacions"]');
+  if (!table) {
+    alert("No s'ha trobat la taula de qualificacions");
+    return
+  }
 
-  let selects = document.querySelectorAll("select"); /**:not([disabled]) */
+  let selects = table.querySelectorAll("select"); /**:not([disabled]) */
+
   selects.forEach(select => {
+
+    //TODO Mirar si select esta desabilitat
       let optionExists = Array.from(select.options).some(option => option.value === state);
       let event;
       if (optionExists  && ( !select.value || force) ) {
@@ -150,13 +240,29 @@ function modifySelect(state, force) {
           select.dispatchEvent(event);
       }
 
+   
       optionExists = Array.from(select.options).some(option => option.value === "string:PQ");
-      if (optionExists && ( !select.value || force)) {
+      let input = select.parentElement.querySelector("input");
+      let hasQualification = input && input.value && input.value != "";
+
+      //Possara pendent si no te nota, exeptuant si es força
+      if (optionExists && ( (!select.value && !hasQualification) || force) ) {
+
           select.value = "string:PQ";
           event = new Event('change');
           select.dispatchEvent(event);
       }
   });
         
+}
+
+function extractStudentInfo() {
+  const breadcrumbItems = document.querySelectorAll('.breadcrumb-wrapper ol.breadcrumb li');
+  const lastItem = breadcrumbItems[breadcrumbItems.length - 1];
+  if (!lastItem) return "No encontrado";
+
+  const lastItemText = lastItem.textContent.trim();
+  const [code, name] = lastItemText.split(' - ');
+  return { code, name };
 }
 
